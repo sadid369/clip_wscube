d578bcea83eecedf7afb5ad9a2fdb15b8cf8827b
sadid369 <sadid.jones@gmail.com>
2023-11-11 20:13:38 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/clip_wscube/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clip_wscube
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/backdrop_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BackdropPage extends StatefulWidget {
+  const BackdropPage({Key? key}) : super(key: key);
+
+  @override
+  _BackdropPageState createState() => _BackdropPageState();
+}
+
+class _BackdropPageState extends State<BackdropPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
